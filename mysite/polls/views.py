63cdc58b9883aaed34890d96dbf37ac27b8d0ad6
robin <robinnsheen@berkeley.edu>
2022-08-01 16:45:03 -0700

    done

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -9,8 +9,6 @@ from django.utils import timezone
 from .models import Question, Choice
 
 
-
-
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
